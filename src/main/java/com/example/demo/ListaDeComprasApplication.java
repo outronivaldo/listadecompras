6ec9de37da40eade22aa85a56a2569108b2fa7b2
outronivaldo <contatonivaldomanoel@gmail.com>
2019-05-20 22:19:42 -0300

    Primeiro commit

---
+++ src/main/java/com/example/demo/ListaDeComprasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaDeComprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaDeComprasApplication.class, args);
+	}
+
+}
