118a593f79b75f6954bab346fff40244d34dfd7f
outronivaldo <contatonivaldomanoel@gmail.com>
2019-05-27 22:43:00 -0300

    Removido os testes da classe Application

--- src/main/java/com/example/demo/ListaDeComprasApplication.java
+++ src/main/java/com/example/demo/ListaDeComprasApplication.java
@@ -1,21 +1,12 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import domain.ListaDeCompras;
-import repositories.ListaDeComprasRepository;
-
-
 
 @SpringBootApplication
 public class ListaDeComprasApplication implements CommandLineRunner {
-	
-	@Autowired
-	private ListaDeComprasRepository listaRep;
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListaDeComprasApplication.class, args);
@@ -23,10 +14,7 @@ public class ListaDeComprasApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ListaDeCompras lista1 = new ListaDeCompras();
-		lista1.setNome("Lista teste");
-		
-		listaRep.save(lista1);
+
 	}
 
 }
